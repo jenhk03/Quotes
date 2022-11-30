3f110c26d2d7282e803395f7c6ecfe334da92bd5
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2022-11-30 15:10:22 +0700

    Update

---
+++ app/src/main/java/com/example/quotes/API/APIRequestData.java
@@ -0,0 +1,12 @@
+package com.example.quotes.API;
+
+import com.example.quotes.Model.QuotesModel;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIRequestData
+{
+    @GET("quotes")
+    Call<List<QuotesModel>> requestData();
+}
--- app/src/main/java/com/example/quotes/Model/Model.java
+++
@@ -1,5 +0,0 @@
-package com.example.quotes.Model;
-
-public class Model
-{
-}
